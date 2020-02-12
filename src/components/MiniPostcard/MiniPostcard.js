1c247961f405c9e34ac2ff1d7eea467c02f1a94c
Beth Dunford <eag732@gmail.com>
2020-02-12 12:35:57 -0600

    AllPosts PostCard and MiniPostcard components updated and fully functional

--- src/components/MiniPostcard/MiniPostcard.js
+++ src/components/MiniPostcard/MiniPostcard.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import PopUp from '../PopUp/PopUp';
 
 class MiniPostcard extends Component {
     constructor() {
@@ -13,10 +13,24 @@ class MiniPostcard extends Component {
 
     render() {
         return (
-            <div>
-                <h1>Mini Postcard</h1>
-                
-
+            <div className="dog-container">
+                <div className="dog-container-content" key={this.props.post_id}>
+                    <PopUp
+                        user={this.props.user_id}
+                        pic={this.props.pic}
+                        post_name={this.props.post_name}
+                        breed={this.props.breed}
+                        age={this.props.age}
+                        gender={this.props.gender}
+                        fixed={this.props.fixed}
+                        rating={this.props.rating}
+                        org_name={this.props.org_name}
+                        bio={this.props.bio}
+                    />
+                    <h5 className="dog-name">{this.props.post_name}</h5>
+                    <h5 className="dog-gender">{this.props.gender}</h5>
+                    <h5 className="dog-fixed">{this.props.fixed}</h5>
+                </div>
             </div>
         )
     }
