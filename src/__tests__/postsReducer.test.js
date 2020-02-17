0a1cf3e4d5ab094637e4c70aaa0a1b21008c588a
Beth Dunford <eag732@gmail.com>
2020-02-17 09:38:09 -0600

    My 10 tests are finished and passing

--- src/__tests__/postsReducer.test.js
+++ src/__tests__/postsReducer.test.js
@@ -1,15 +1,5 @@
 import { getCurrentPost, getAllPostsByUserId, getRandomPosts, addPostCount } from "../redux/reducers/postsReducer";
 
-// import React from "react";
-// import ReactDOM from "react-dom";
-// import { act, Simulate } from "react-dom/test-utils";
-// import Login from "../components/Login/Login";
-// import App from "../App";
-// import { Provider } from "react-redux";
-// import configureMockStore from "redux-mock-store";
-// import { shallow } from 'enzyme';
-// import renderer from 'react-test-renderer';
-
 //Beth Test
 describe('getCurrentPost', () => {
     test('passes when value is NaN', () => {
