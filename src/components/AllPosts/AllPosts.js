8f87bf28c27ae41d8d0df32bd9407e0a43cf3888
Beth Dunford <eag732@gmail.com>
2020-02-19 10:32:36 -0600

    More styling added, also getting R and Js updates

--- src/components/AllPosts/AllPosts.js
+++ src/components/AllPosts/AllPosts.js
@@ -12,16 +12,16 @@ class AllPosts extends Component {
     }
 
     componentDidMount() {
-        
+
     }
 
 
     render() {
         const { posts } = this.props;
-        
+
         const postsMapped = posts.map((post, i) => {
             return (
-                <div key={i}>
+                <div key={i} className="all-posts-container">
                     <MiniPostcard
                         user={post.user_id}
                         post_id={post.post_id}
@@ -40,8 +40,10 @@ class AllPosts extends Component {
             )
         })
         return (
-            <div>
-                {postsMapped}
+            <div id="all-posts-main">
+                <div id="all-posts-page">
+                    {postsMapped}
+                </div>
             </div>
         )
     }
